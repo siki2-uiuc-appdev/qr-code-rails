bfd62bd16dcb99d2cf528075715049ad02cee12c
Raghu Betina <Raghu.Betina@chicagobooth.edu>
2022-10-06 15:54:30 +0000

    Add other solutions

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -1,6 +1,27 @@
 class SolutionsController < ApplicationController
-  def wifi_code
+  def text_qr
+    @user_text = params.fetch("content")
+
+    qrcode = RQRCode::QRCode.new(@user_text)
+
+    @svg = qrcode.as_svg
+
+    render({ :template => "solutions/qr_for_text.html.erb" })
+  end
+
+  def url_qr
+    @user_url = params.fetch("page")
+
+    qrcode = RQRCode::QRCode.new(@user_url)
+
+    @svg = qrcode.as_svg
+
+    render({ :template => "solutions/qr_for_url.html.erb" })
+  end
+
+  def wifi_qr
     @network_name = params.fetch("ssid")
+
     @network_password = params.fetch("pw")
 
     text_to_encode = "WIFI:T:WPA;S:#{@network_name};P:#{@network_password};;"
@@ -9,6 +30,20 @@ class SolutionsController < ApplicationController
 
     @svg = qrcode.as_svg
 
-    render({ :template => "solutions/wifi.html.erb" })
+    render({ :template => "solutions/qr_for_wifi.html.erb" })
+  end
+
+  def sms_qr
+    @phone_number = params.fetch("phone").gsub(/\s+/, "")
+
+    @starter_message = params.fetch("message")
+
+    text_to_encode = "SMSTO:#{@phone_number}:#{@starter_message}"
+
+    qrcode = RQRCode::QRCode.new(text_to_encode)
+
+    @svg = qrcode.as_svg
+
+    render({ :template => "solutions/qr_for_sms.html.erb" })
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
-
+  # Write your routes here. URLs to support:
+  
+  # /text
+  # /url
+  # /wifi
+  # /sms
 
   # Solutions below.
 
-  get("/solutions/wifi", { :controller => "solutions", :action => "wifi_code" })
+  get("/solutions/text", { :controller => "solutions", :action => "text_qr" })
+
+  get("/solutions/url", { :controller => "solutions", :action => "url_qr" })
+
+  get("/solutions/wifi", { :controller => "solutions", :action => "wifi_qr" })
+
+  get("/solutions/sms", { :controller => "solutions", :action => "sms_qr" })
 
 end
